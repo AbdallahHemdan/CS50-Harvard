dceabd27bf3ecb68e1251841213122462c9a6862
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-26 07:42:21 +0200

    Create mario_hard.c

---
+++ Problem Set 1/mario/Hard/mario_hard.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <cs50.h>
+/*
+Height: 8
+       #  #
+      ##  ##
+     ###  ###
+    ####  ####
+   #####  #####
+  ######  ######
+ #######  #######
+########  ########
+  */
+/*
+Take Care about The User bad input
+Height: -1
+Height: 0
+Height: 42
+Height: 50
+Height: 4
+   #  #
+  ##  ##
+ ###  ###
+####  ####
+  */
+void Mario_Show(int heigth) {
+  for (int i = 0; i < heigth; i++) {
+    for (int j = 0; j < heigth - i - 1; j++) {
+      printf("");
+    }
+    for (int k = 0; k < i + 1; k++) {
+      printf("#");
+    }
+    printf("  ");
+    for (int k = 0; k < i + 1; k++) {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
+int main() {
+  int heigth;
+  do {
+    heigth = get_int("Height: ");
+  } while (heigth < 1 || heigth > 8);
+  Mario_Show(heigth);
+}
