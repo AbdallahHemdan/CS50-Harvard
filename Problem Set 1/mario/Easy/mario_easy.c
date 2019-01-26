b5b1905c6258758974a6e03a0da2e5715f6144e8
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-01-26 07:26:33 +0200

    Update mario_easy.c

--- Problem Set 1/mario/Easy/mario_easy.c
+++ Problem Set 1/mario/Easy/mario_easy.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <cs50.h>
+/*
+Example : 
 
+Height: 8
+       #
+      ##
+     ###
+    ####
+   #####
+  ######
+ #######
+########
+  */
+/*
+Take Care about The User bad input
+Height: -1
+Height: 0
+Height: 42
+Height: 50
+Height: 4
+   #
+  ##
+ ###
+####
+  */
 void Mario_Show(int heigth) {
   for (int i = 0; i < heigth; i++) {
     for (int j = 0; j < heigth - i - 1; j++) {
